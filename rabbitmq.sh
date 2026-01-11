c8548e076887cde1510e06ea1e65d70858f03ec1
Akhil-Godavari <akhilgodavari.21@gmail.com>
2026-01-11 13:47:10 -0600

    rabbitmq.sh adding

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,6 +10,8 @@ N="\e[37m"
 Logs_Folder="/var/log/shell-roboshop"
 Script_Name=$( echo $0 | cut -d "." -f1 )
 Log_File="$Logs_Folder/$Script_Name.log"
+Script_Dir=$(PWD)
+
 
 
 mkdir -p $Logs_Folder
@@ -29,21 +31,23 @@ VALIDATE(){
     fi
 }
 
-dnf install mysql-server -y &>>Log_File
-VALIDATE $? "Installing MYSQL Server"
+cp $Script_Dir/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Adding RabbitMQ Repo"
 
-systemctl enable mysqld &>>Log_File
-VALIDATE $? "Enabling MYSQL Server"
+dnf install rabbitmq-server -y &>>Log_File
+VALIDATE $? " Installing RabbitMQ"
 
-systemctl start mysqld  &>>Log_File
-VALIDATE $? "Starting MYSQL Server"
+systemctl enable rabbitmq-server &>>Log_File
+VALIDATE $? " Enabling RabbitMQ"
 
-mysql_secure_installation --set-root-pass RoboShop@1
-VALIDATE $? "Setting Up root password"
+systemctl start rabbitmq-server &>>Log_File
+VALIDATE $? " Starting RabbitMQ"
 
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? " Setting Up the Permissions"
 
 Start_Time=$(date +%s)
 End_Time=$(date +%s)
 Total_Time=$(($End_Time - $Start_Time))
 echo -e " Script Excution time: $Y $Total_Time Seconds $N"
-
